97ae8edc2f683a77c2e6698b58ebcf3f150a873d
nikumpf <nik@CLK00012534.orfnet.at>
2025-02-06 10:09:50 +0100

    Amplify

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+//import type { Schema } from '../amplify/data/resource';
+import type { Schema } from '../../amplify/data/resource';
+import { generateClient } from 'aws-amplify/data';
+const client = generateClient<Schema>();
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
