3c1bc00201523030426eb86fecaa3fdff01bbd48
nikumpf <nik@CLK00012534.orfnet.at>
2025-02-07 15:12:27 +0100

    Channel Component added.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ChannelsConfigurationComponent } from './components/channels-configuration/channels-configuration.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent, pathMatch: 'full' },
-    /*
     { path: 'dashboard', component: DashboardComponent },
     { path: 'channel/:id', component: ChannelsConfigurationComponent },
-    { path: 'counter', component: CounterComponent },
-    { path: 'fetch-data', component: FetchDataComponent },
-     */
 ];
