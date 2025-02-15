c4323b84ca60f795c33f78635078a981463e8869
Ronald Macek <Ronald.Macek.extern@orfnet.at>
2025-02-15 12:45:48 +0100

    ChannelConfigurationComponent: form

--- src/app/components/channels-configuration/channels-configuration.component.ts
+++ src/app/components/channels-configuration/channels-configuration.component.ts
@@ -5,18 +5,21 @@ import { ChannelsService } from '../../services/channels.service';
 import { IChannel } from '../../models/channel';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-channels-configuration',
   templateUrl: './channels-configuration.component.html',
   styleUrl: './channels-configuration.component.scss',
   standalone: true,
-  imports: [CommonModule, ButtonModule]
+  imports: [CommonModule, FormsModule, ButtonModule]
 })
 export class ChannelsConfigurationComponent implements OnInit, OnDestroy {
   _channels: IChannel[] = [];
   channelId: string = '';
   currentChannel?: IChannel = undefined;
+  tempChannel: Partial<IChannel> = {};
+  editingField: string | null = null;
 
   private _unsubscribeAll: Subject<any> = new Subject<any>();
   
@@ -42,5 +45,38 @@ export class ChannelsConfigurationComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this._unsubscribeAll.next(null);
     this._unsubscribeAll.complete();
+  }
+
+  toggleEdit(field: keyof IChannel): void {
+    if (this.editingField === field) {
+      this.saveChanges(field);
+    } else {
+      this.editingField = field;
+      this.tempChannel[field as keyof IChannel] = this.currentChannel ? (this.currentChannel[field as keyof IChannel] as any) : undefined;
+    }
+  }
+
+  cancelEdit(): void {
+    this.editingField = null;
+    this.tempChannel = { ...this.currentChannel };  // Setzt die temporären Werte zurück
+  }
+
+  saveChanges<T extends keyof IChannel>(field: T): void {
+    if (this.currentChannel) {
+      this.currentChannel[field] = this.tempChannel[field] as IChannel[T];
+      this.updateChannel(this.currentChannel);
+    }
+    this.editingField = null;
+  }
+
+  updateChannel(channel: IChannel) {
+    this.channelsService.updateChannel(channel).subscribe(
+      updatedChannel => {
+        console.log('Channel updated:', updatedChannel);
+        // Optional: Die Kanäle neu laden
+        this.channelsService.getChannels();
+      },
+      error => console.error('Update failed', error)
+    );
   }
 }
